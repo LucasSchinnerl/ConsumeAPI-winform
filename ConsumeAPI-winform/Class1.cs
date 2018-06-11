a1a86924aaf7667c4c8153f3ad552025eae85b78
G. Reiter <guntbert@gmx.at>
2018-06-11 16:17:11 +0200

    Create Class for deserialization

---
+++ ConsumeAPI-winform/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeAPI_winform
+{
+    public class Rootobject
+    {
+        public Note[] notes { get; set; }
+    }
+
+    public class Note
+    {
+        public int noteid { get; set; }
+        public string message { get; set; }
+    }
+
+}
